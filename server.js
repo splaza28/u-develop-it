8ee31377defef60f1fd80ae44867b9658039f73e
splaza28 <global-splaza28@gmail.com>
2022-01-16 09:08:22 -0600

    adding js file

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+
+
+  
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
