c1c1020001601c89affe3954174b43e2902a1afb
splaza28 <global-splaza28@gmail.com>
2022-01-16 12:51:07 -0600

    updates to server js file

--- server.js
+++ server.js
@@ -1,16 +1,57 @@
 const express = require('express');
 const PORT = process.env.PORT || 3002;
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+
+      user: 'root',
+      // Your MySQL password
+      password: 'Boymom1921!!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 
 
 
 
 
+  //db.query(`SELECT * FROM candidates`, (err, rows) => {
+    //console.log(rows);
+  //});
 
+//   db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
   
 
 app.use((req, res) => {
